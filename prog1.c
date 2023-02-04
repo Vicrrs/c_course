bd9442a644d4f0f07f3ea06edd01ac39572c6de2
Vicrrs <enricofermi2019@gmail.com>
2023-02-04 19:13:38 -0400

    first exercises

---
+++ prog1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+	//código que será executado
+	printf("Hello, world!\n"); // função de saída de dados
+	
+}
+
---
+++ prog3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+	
+	printf("Olá, tudo bem? Já programa há 3 anos.\n");
+	
+	printf("Valor inteiro: %d.\n", 10);
+	printf("Valor real: %f.\n", 3.14159265);
+	printf("Valor real com apenas duas casas: %.2f.\n", 3.14159265);
+	printf("Dado de texto: %c.\n", 'A');
+	printf("Dado de texto: %s.\n", "TESTANDO");
+}
+
