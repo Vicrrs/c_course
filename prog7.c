c26099805dcf50c49cefbad10852f717b2db3a44
Vicrrs <enricofermi2019@gmail.com>
2023-02-05 12:50:40 -0400

    arithmetic operations

---
+++ prog7.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+    int A, B, soma, subr, mult, divis;
+
+    printf("Digite o primeiro valor: ");
+    scanf("%d",&A);
+    printf("Digite o segundo valor: ");
+    scanf("%d",&B);
+
+    soma = A + B;
+    subr = A - B;
+    mult = A * B;  
+    divis = A / B;
+
+    printf("\n");
+    printf("Resultados.\n");
+    printf("Soma: %d.\n", soma);
+    printf("Subtração: %d.\n", subr);
+    printf("Multicação: %d.\n", mult);
+    printf("Divisão: %d.\n", divis);
+
+}
